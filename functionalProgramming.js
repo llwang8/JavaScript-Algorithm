ca76ec1ff1792b1bbf888e5b6b9963ad2be633c8
llwang8 <wangxbl56@gmail.com>
2016-03-17 12:55:11 -0400

    functional JS programming exercises 27-32

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -1393,6 +1393,194 @@ function() {
 
 }
 /*====================================
+Exercise 27: Stock Ticker
+
+Let's try an easier question. Let's say we have a collection of all of the prices for
+NASDAQ stocks over time. Every time the price of a stock changes on the NASDAQ ticker an
+entry is added to this collection. Let's say that ten days ago you bought shares in Microsoft,
+and now you want to print all of the MSFT share prices since then. Filter the collection for
+MSFT trades starting from ten days ago and print each price record (including the time stamp)
+using the print() function. Note: this is not a trick question. It's as easy as it seems.
+
+// The pricesNASDAQ collection looks something like this...
+var pricesNASDAQ = [
+    // ... from the NASDAQ's opening day
+    {name: "ANGI", price: 31.22, timeStamp: new Date(2011,11,15) },
+    {name: "MSFT", price: 32.32, timeStamp: new Date(2011,11,15) },
+    {name: "GOOG", price: 150.43, timeStamp: new Date(2011,11,15)},
+    {name: "ANGI", price: 28.44, timeStamp: new Date(2011,11,16)},
+    {name: "GOOG", price: 199.33, timeStamp: new Date(2011,11,16)},
+    // ...and up to the present.
+];
+
+=====================================*/
+function(pricesNASDAQ, printRecord) {
+    var microsoftPrices,
+        now = new Date(),
+        tenDaysAgo = new Date( now.getFullYear(), now.getMonth(), now.getDate() - 10);
+
+    // use filter() to filter the trades for MSFT prices recorded any time after 10 days ago
+    microsoftPrices = pricesNASDAQ.filter(function(priceRecord) {    // finish this expression
+        return priceRecord.name === "MSFT" && priceRecord.timeStamp > tenDaysAgo;
+  });
+
+  microsoftPrices.forEach(function(priceRecord) {
+            printRecord(priceRecord);
+    });
+}
+
+
+/*====================================
+Exercise 28: Subscribing to an event
+
+You're probably used to thinking about events as a list of handlers stored in an object.
+In this example, we subscribe to a button click event and then unsubscribe the first time
+the button is clicked.
+
+=====================================*/
+function(button) {
+    // the button click handler
+    var handler = function(ev) {
+        // Unsubscribe from the button event.
+        button.removeEventListener("click", handler);
+
+        alert("Button was clicked. Unsubscribing from event.");
+    };
+
+    // add the button click handler
+    button.addEventListener("click", handler);
+}
+
+/*====================================
+Exercise 29: Traversing an Event
+
+Subscribing to an Event and traversing an Array are fundamentally the same operation.
+The only difference is that Array traversal is synchronous and completes, and Event traversal
+is asynchronous and never completes. If we convert a button click Event to an Observable object,
+we can use forEach() to traverse the Event.
+
+=====================================*/
+function(button) {
+    var buttonClicks = Observable.fromEvent(button, "click");
+
+    // In the case of an Observable, forEach returns a subscription object.
+    var subscription =
+        buttonClicks.
+            forEach(function(clickEvent) {
+                alert("Button was clicked. Stopping Traversal.");
+
+                // Stop traversing the button clicks
+                subscription.dispose();
+            });
+}
+
+/*====================================
+Exercise 30: Completing Sequences with take()
+Let's repeat the previous exercise, in which we listened for a single button click and
+then unsubscribed. This time let's use the take() function to complete the sequence after
+the button is clicked.
+
+=====================================*/
+function(button) {
+    var buttonClicks = Observable.fromEvent(button, "click");
+
+    // Use take() to listen for only one button click
+    // and unsubscribe.
+    buttonClicks.
+        take(1).
+        forEach(function() {
+            alert("Button was clicked once. Stopping Traversal.");
+        });
+}
+
+/*====================================
+Exercise 31: Completing sequences with takeUntil()
+
+Have you ever wanted to unsubscribe from one Event when another Event fires? Observable's
+takeUntil() function is a convenient way of completing a sequence when another Event occurs.
+Here's how takeUntil() works:
+
+=====================================*/
+function(pricesNASDAQ, printRecord, stopButton) {
+    var stopButtonClicks = Observable.fromEvent(stopButton, "click");// ----- To finish this expression, use Observable.fromEvent to convert the "click" event on the stop button to an Observable
+        microsoftPrices =
+            pricesNASDAQ.
+                filter(function(priceRecord) {
+                    return priceRecord.name === "MSFT";
+                }).takeUntil(stopButtonClicks);
+                // ----- To finish this expression, use takeUntil to complete the sequence when stopButtonClicks fires.
+
+    microsoftPrices.
+        forEach(function(priceRecord) {
+            printRecord(priceRecord);
+        });
+}
+
+/*====================================
+Exercise 32: Creating a mouse drag event
+
+Remember the exercise we solved earlier? The one in which we retrieved all the movies with a
+rating of 5.0 from an array of movie lists? If we were to describe the solution in pseudocode
+it might read something like this...
+
+"For every movie list, retrieve only those videos with a rating of 5.0"
+
+=====================================*/
+
+
+
+
+
+
+/*====================================
+
+
+=====================================*/
+
+
+
+
+
+
+/*====================================
+
+
+=====================================*/
+
+
+
+
+
+
+/*====================================
+
+
+=====================================*/
+
+
+
+
+
+/*====================================
+
+
+=====================================*/
+
+
+
+
+
+/*====================================
+
+
+=====================================*/
+
+
+
+
+
+
+/*====================================
 
 
 =====================================*/
@@ -1419,3 +1607,11 @@ function() {
 =====================================*/
 
 
+
+
+
+
+/*====================================
+
+
+=====================================*/
