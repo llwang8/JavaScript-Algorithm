5375b949bde599a88ec9241749f7d86bd5f457fe
llwang8 <wangxbl56@gmail.com>
2016-03-24 19:33:08 -0400

    in flight matching two movies

--- interviewCake1-25.js
+++ interviewCake1-25.js
@@ -549,22 +549,57 @@ function findRotationIndex(words){
         if (floorIndex + 1 == ceilingIndex){
             break;
         }
+        if (ceilingIndex == words.length){
+            return 0;
+        }
     }
     return ceilingIndex;
 }
 
-
-
-
-
-
-
-
 /*==================================
+#14 Inflight Entainment
+You've built an in-flight entertainment system with on-demand movie streaming.
+Users on longer flights like to start a second movie right when their first one ends, but
+hey complain that the plane usually lands before they can see the ending. So you're
+building a feature for choosing two movies whose total runtimes will equal the exact flight
+length.
 
+Write a function that takes an integer flightLength (in minutes) and an array of integers
+movieLengths (in minutes) and returns a boolean indicating whether there are two numbers
+in movieLengths whose sum equals flightLength.
+
+When building your function:
+
+Assume your users will watch exactly two movies
+Don't make your users watch the same movie twice
+Optimize for runtime over memory
 ====================================*/
+function inFlightMovies(flightLength, arrMovies){
+    if (flightLength === 0 || arrMovies.length === 0){
+        throw new Error ("Invalid Entry");
+    }
+    var result = [];
+    var otherMovieLength = 0;
+    for (var i=0; i<arrMovies.length; i++){
+        otherMovieLength = flightLength - arrMovies[i];
+        if (arrMovies.indexOf(otherMovieLength) > 0 && arrMovies.indexOf(otherMovieLength) != i){
+            return true;
+        }
+    }
+    return false;
+}
+function getTwoMoviesToFillUpTime(movieLengths, flightLength){
+    var movieLengthSeen = new Set();
 
-
+    for (var i=0; i<movieLengths.length; i++){
+        var firstMovieLength = movieLengths[i];
+        if (movieLengthSeen.has(flightLength -  firstMovieLength)){
+            return true;
+        }
+        movieLengthSeen.push(firstMovieLength); ///???
+    }
+    return false;
+}
 
 
 /*==================================
