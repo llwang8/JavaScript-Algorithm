0dc793fba10f4d01eddac013c65721e882b7f39f
llwang8 <wangxbl56@gmail.com>
2016-03-24 22:21:20 -0400

    cake thief

--- interviewCake1-25.js
+++ interviewCake1-25.js
@@ -603,19 +603,122 @@ function getTwoMoviesToFillUpTime(movieLengths, flightLength){
 
 
 /*==================================
+#15. Commute nth fibonacci number
+Write a function fib() that a takes an integer
+n
+n and returns the
+n
+nth fibonacci ↴ number.
+Let's say our fibonacci series is 0-indexed and starts with 0. So:
 
+fib(0); // => 0
+fib(1); // => 1
+fib(2); // => 1
+fib(3); // => 2
+fib(4); // => 3
+...
 ====================================*/
+function Fibber(n){
+    this.memo = {};
+}
 
+Fibber.prototype.fibonacciNumbers = function(n){
+    if (n < 0){
+        throw new Error("Index was nagitive.  Invalid entry.")
+    }
+    if (n === 0 || n === 1){
+        return n;
+    }
+    if (this.memo.hasOwnProperty(n)){
+        return memo[n];
+    }
 
+    var result = fibonacciNumbers(n-1) + fibonacciNumbers(n-2);
+    this.memo[n] = result;
 
+}
+// botton-up approach
+function fibBottomUp(n){
+    if (n < 0){
+        throw new Error("Index was nagitive.  Invalid entry.")
+    }
+    var pre = 1;
+    var prepre = 0;
+    for (var i = 0, i<n; i++){
+        current = pre + prepre;
+        prepre = pre;
+        pre = current;
+    }
+    return current;
+}
 
 /*==================================
+#16. The Cake thief
+You are a renowned thief who has recently switched from stealing precious metals to
+stealing cakes because of the insane profit margins. You end up hitting the jackpot,
+breaking into the world's largest privately owned stock of cakes—the vault of the Queen
+of England.
+While Queen Elizabeth has a limited number of types of cake, she has an unlimited supply
+of each type.
 
+Each type of cake has a weight and a value, stored in an object with two properties:
+
+weight: the weight of the cake in kilograms
+value: the monetary value of the cake in British pounds
+For example:
+
+  // weighs 7 kilograms and has a value of 160 pounds
+{weight: 7, value: 160}
+
+// weighs 3 kilograms and has a value of 90 pounds
+{weight: 3, value: 90}
+
+You brought a duffel bag that can hold limited weight, and you want to make off with the
+most valuable haul possible.
+
+Write a function maxDuffelBagValue() that takes an array of cake type objects and a weight
+capacity, and returns the maximum monetary value the duffel bag can hold.
+
+For example:
+
+  var cakeTypes = [
+    {weight: 7, value: 160},
+    {weight: 3, value: 90},
+    {weight: 2, value: 15},
+];
+
+var capacity = 20;
+
+maxDuffelBagValue(cakeTypes, capacity);
+// returns 555 (6 of the middle type of cake and 1 of the last type of cake)
+
+Weights and values may be any non-negative integer. Yes, it's weird to think about cakes
+that weigh nothing or duffel bags that can't hold anything. But we're not just super
+mastermind criminals—we're also meticulous about keeping our algorithms flexible and
+comprehensive.
 ====================================*/
+function maxDuffelBagValue(cakeTypes, weightCapacity){
+    var maxValueAtCapacity = [];
+    for (var i=0; i<cakeTypes.length; i++){
+        maxValueAtCapacity[i] = 0;
+    }
 
-
-
-
+    for (var currentCapacity=0; currentCapacity<weightCapacity; currentCapacity++){
+        var currentMaxValue = 0;
+        for (var j=0; j<cakeTypes.length; j++){
+            var currentCake = cakeTypes[i];
+            if(cakeType.weight === 0 && cakeType.value != 0){
+                return Infinity;
+            }
+            if (cakeType.weight < currentCapacity){
+                var maxValueUsingCake = cakeType.value + maxValueAtCapacity[weightCapacity - cakeType.weight];
+            }
+            currentMaxValue = Math.max(maxValueUsingCake + currentMaxValue);
+        }
+        maxValueAtCapacity[currentCapacity] = currentMaxValue;
+    }
+    return maxValueAtCapacity[weightCapacity];
+}
 
 /*==================================
 
