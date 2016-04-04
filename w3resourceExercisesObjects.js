64fc23167e8d47672acf90596680ac15c5820b1e
llwang8 <wangxbl56@gmail.com>
2016-04-04 15:03:00 -0400

    finish w3resources Objects

--- w3resourceExercisesObjects.js
+++ w3resourceExercisesObjects.js
@@ -3,44 +3,48 @@
 
 /*==============================
 
-1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
-Sample object :
+1. Write a JavaScript program to list the properties of a JavaScript object.
 var student = {
 name : "David Rayy",
 sclass : "VI",
 rollno : 12 };
 Sample Output : name,sclass,rollno
 ==============================*/
-
+var student = {name : "David Rayy", sclass : "VI", rollno : 12};
+console.log(Object.keys(student));
 
 
 /*==============================
 
-2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Go to the editor
+2. Write a JavaScript program to delete the rollno property from the following object.
+Also print the object before or after deleting the property.
 Sample object :
 var student = {
 name : "David Rayy",
 sclass : "VI",
 rollno : 12 };
 ==============================*/
-
+console.log(student);
+delete student.rollno;
+console.log(student);
 
 
 /*==============================
 
-3. Write a JavaScript program to get the length of an JavaScript object. Go to the editor
+3. Write a JavaScript program to get the length of an JavaScript object.
 Sample object :
 var student = {
 name : "David Rayy",
 sclass : "VI",
 rollno : 12 };
 ==============================*/
-
+return Object.keys(student).length;
 
 
 /*==============================
 
-4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. Go to the editor
+4. Write a JavaScript program to display the reading status (i.e. display book name,
+author name and reading status) of the following books.
 
 var library = [
    {
@@ -59,40 +63,73 @@ var library = [
        readingStatus: false
    }];
 ==============================*/
-
-
+var result = library.map(function(book){
+  if (book.readingStatus) {
+    return "Already read: " + book.title;
+  }else {
+    return "You still need to read " + book.title;
+  }
+});
+console.log(result);
 
 /*==============================
 
-5. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes. Go to the editor
+5. Write a JavaScript program to get the volume of a Cylinder with four decimal
+places using object classes.
 Volume of a cylinder : V = πr2h
 where r is the radius and h is the height of the cylinder.
 ==============================*/
-
-
-
+function Cylinder(r, h){
+  this.radius = r;
+  this.height = h;
+  this.volume = function(){
+    return (Math.PI * this.radius * 2 * this.height).toFixed(4);
+  };
+}
 /*==============================
 
-6. Write a Bubble Sort algorithm in JavaScript. Go to the editor
-Note : Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted,
+6. Write a Bubble Sort algorithm in JavaScript.
+Note : Bubble sort is a simple sorting algorithm that works by repeatedly
+stepping through the list to be sorted,
 Sample Data : [6,4,0, 3,-2,1]
 Expected Output : [-2, 0, 1, 3, 4, 6]
 ==============================*/
-
-
+function bubbleSort(arr){
+  var i, j,temp;
+  var len = arr.length;
+  for (i=0; i<len; i++){
+    for (j=1; j<len-i; j++){
+      if (arr[j-1] > arr[j]){
+        temp = arr[j-1];
+        arr[j-1]= arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([6,4,0, 3,-2,1]));
 
 /*==============================
 
-7. Write a JavaScript program which returns a subset of a string. Go to the editor
+7. Write a JavaScript program which returns a subset of a string.
 Sample Data : dog
 Expected Output : ["d", "do", "dog", "o", "og", "g"]
 ==============================*/
-
-
+String.prototype.subStr = function(str){
+  var result = [];
+  for (var i=0; i<this.length; i++){
+    for (var j=i+i; j<this.length; j++){
+      result.push(this.slice(i, j));
+    }
+  }
+  return result;
+}
+console.log("dog".subStr());
 
 /*==============================
 
-8. Write a JavaScript program to create a Clock. Go to the editor
+8. Write a JavaScript program to create a Clock.
 Note : The output will come every second.
 Expected Console Output :
 "14:37:42"
@@ -102,19 +139,53 @@ Expected Console Output :
 "14:37:46"
 "14:37:47"
 ==============================*/
+function MyClock(){
+  this.curDate = new Date();
+  this.hours = this.curDate.getHours();
+  this.minutes = this.curDate.getMinutes();
+  this.seconds = this.curDate.getSeconds();
+}
+MyClock.prototype.run = function(){
+  setInterval(this.update.bind(this), 1000);
+}
+MyClock.prototype.update = function(){
+  this.updateTime(1);
+  console.log(this.hours + this.minutes + this.seconds);
+}
+MyClock.prototype.updateTime = funciton(sec){
+  this.seconds += sec;
+  if (this.seconds >= 60){
+    this.minutes ++;
+    this.seconds = 0;
+  }
+  if (this.minutes >= 60){
+    this.hours ++;
+    this.minutes =0;
+  }
+  if (this.hours >= 24){
+    this.hours = 0;
+  }
+}
 
+var clock = new MyClock();
+clock.run();
 
 
 /*==============================
 
-9. Write a JavaScript program to calculate the area and perimeter of a circle. Go to the editor
-Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
+9. Write a JavaScript program to calculate the area and perimeter of a circle.
+Note : Create two methods to calculate the area and perimeter. The radius of the
+circle will be supplied by the user.
 ==============================*/
-
+function Circle(r){
+  this.radius = r;
+  this.area = Math.PI * r * r;
+  this.perimeter = 2 * r * Math.PI;
+}
 
 
 /*==============================
-10. Write a JavaScript program to sort an array of JavaScript objects. Go to the editor
+10. Write a JavaScript program to sort an array of JavaScript objects.
 Sample Object :
 
 var library = [
@@ -149,23 +220,69 @@ Expected Output :
   title: "Bill Gates"
 }]
 ==============================*/
+function sortBy(byField, reverse, process){
+  return library.sort(function(a, b){
+    if (process != null){
+      return reverse ? process(b[byField]) - process(a[byField]) : process(a[byField]) - process(b[byField]);
+    }else {
+      return reverse ? b[byField] - a[byField] : a[byField] - b[byField];
+    }
+  });
+}
 
-
+var newobj = library.sort(sortBy('libraryID', true, parseInt));
+console.log(newobj);
 
 /*==============================
-11. Write a JavaScript function to print all the methods in an JavaScript object. Go to the editor
+11. Write a JavaScript function to print all the methods in an JavaScript object.
 Test Data :
 console.log(all_properties(Array));
 ["length", "name", "arguments", "caller", "prototype", "isArray", "observe", "unobserve"]
 ==============================*/
+function printObjMethods(obj){
+    console.log(Object.getPropertyNames(obj));
 
-
+}
+console.log(printObjMethods(Math));
+console.log(printObjMethods(Array));
 
 /*==============================
 12. Write a JavaScript function to parse an URL.
 
 ==============================*/
+function parse_URL(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function () {
+            var ret = {},
+                seg = a.search.replace(/^\?/, '').split('&'),
+                len = seg.length,
+                i = 0,
+                s;
+            for (; i < len; i++) {
+                if (!seg[i]) {
+                    continue;
+                }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^\/])/, '/$1'),
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+    };
+}
 
+console.log(parse_URL('https://github.com/pubnub/python/search?utf8=%E2%9C%93&q=python'));
 
 
 /*==============================
--- w3resourceExercisesValidation.js
+++ w3resourceExercisesValidation.js
@@ -3,74 +3,76 @@
 
 /*==============================
 
-1. Write a JavaScript function to validate whether a given value type is boolean or not. Go to the editor
-
+1. Write a JavaScript function to validate whether a given value type is boolean or not.
 ==============================*/
-
+return value === true || value === false || Object.prototype.toString.call(value) === '[object Boolean';
 
 
 /*==============================
 
-2. Write a JavaScript function to validate whether a given value type is error or not. Go to the editor
-
+2. Write a JavaScript function to validate whether a given value type is error or not.
 ==============================*/
-
+return x != x;
 
 
 /*==============================
 
-3. Write a JavaScript function to validate whether a given value type is NaN or not. Go to the editor
+3. Write a JavaScript function to validate whether a given value type is NaN or not.
 
 ==============================*/
-
+return x != x;
 
 
 /*==============================
 
-4. Write a JavaScript function to validate whether a given value type is null or not. Go to the editor
+4. Write a JavaScript function to validate whether a given value type is null or not.
 
 ==============================*/
-
+return x === null;
 
 
 /*==============================
 
-5. Write a JavaScript function to validate whether a given value is number or not. Go to the editor
-
+5. Write a JavaScript function to validate whether a given value is number or not.
 ==============================*/
-
+return !isNaN(num) && Object.prototype.toString.call(num) === '[object Number]';
 
 
 /*==============================
 
-6. Write a JavaScript function to validate whether a given value is object or not. Go to the editor
+6. Write a JavaScript function to validate whether a given value is object or not.
+==============================*/
+return Object.prototype.toString.call(obj) === '[object Object]'
+return typeof value === 'function' || typeof value === 'object' || !!value;
+
+/*==============================
+
+7. Write a JavaScript function to validate whether a given value type is pure json
+object or not.
 
 ==============================*/
-
+return Object.prototype.toString.call(obj) === '[object Object]';
 
 
 /*==============================
 
-7. Write a JavaScript function to validate whether a given value type is pure json object or not. Go to the editor
-
+8. Write a JavaScript function to validate whether a given value is RegExp or not.
 ==============================*/
-
+return Object.prototype.toString.call(obj) === '[object RegExp]'
 
 
 /*==============================
 
-8. Write a JavaScript function to validate whether a given value is RegExp or not. Go to the editor
-
+9. Write a JavaScript function to validate whether a given value type is char or not.
 ==============================*/
+function is_char(value){
+    if (Object.prototype.toString.call(value) !== '[object String]')
+        return false;
+    return value && value.length === 1;
+}
 
-
-
-/*==============================
-
-9. Write a JavaScript function to validate whether a given value type is char or not. Go to the editor
-
-==============================*/
-
+console.log(is_char('f'));
+console.log(is_char('*'));
 
 
 /*==============================
@@ -78,6 +80,21 @@
 10. Write a JavaScript function to check whether given value types are same or not.
 
 ==============================*/
+function is_sameType(value1, value2) {
+        if(is_nan(value1) || is_nan(value2)) {
+         return is_nan(value1) === is_nan(value2);
+        }
+        return toString.call(value1) === toString.call(value2);
+    }
+
+function is_nan(val)
+        {
+        return val !== val;
+       }
+
+console.log(is_sameType('12', 100));
+console.log(is_sameType('12', '100'));
+console.log(is_sameType(12, 100));
 
 
 
